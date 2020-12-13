2c1c2b703ba107cea41fb2ef8f2880df6a1980b3
Austin Poor <45295232+a-poor@users.noreply.github.com>
2020-12-12 23:23:18 -0500

    Update download-data.sh

--- download-data.sh
+++ download-data.sh
@@ -2,6 +2,6 @@ if [ ! -d data ] ; then mkdir data; fi;
 cd data
 wget -O unsplash-lite-latest.zip https://unsplash.com/data/lite/latest
 unzip unsplash-lite-latest.zip
-rm unsplash-lite-latest.zip
+rm unsplash-lite-latest.zip README.md TERMS.md
 cd -
 
